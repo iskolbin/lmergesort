548681e6b293ee38c75ed5552a58512f67b13318
Ilya Kolbin <iskolbin@gmail.com>
2017-03-31 16:54:27 +0300

    fixed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![Build Status](https://travis-ci.org/iskolbin/lmergesort.svg?branch=master)
+[![Build Status](https://travis-ci.org/iskolbin/lmergesort.svg?branch=master)](https://travis-ci.org/iskolbin/lmergesort)
 [![license](https://img.shields.io/badge/license-public%20domain-blue.svg)](http://unlicense.org/)
 [![MIT Licence](https://badges.frapsoft.com/os/mit/mit.svg?v=103)](https://opensource.org/licenses/mit-license.php)
 
--- mergesort.lua
+++ mergesort.lua
@@ -1,6 +1,6 @@
 --[[
 
- mergesort -- v1.1.1 Lua non-recursive merge sort implementation
+ mergesort -- v1.1.2 Lua non-recursive merge sort implementation
  no warranty implied; use at your own risk
 
  Based on code snipplet taken from StackOverflow
@@ -109,3 +109,5 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
 ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+--]]
